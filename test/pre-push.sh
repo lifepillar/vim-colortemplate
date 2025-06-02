99587b63ecf863fe526bf87c90816c0da55f353d
Lifepillar <lifepillar@lifepillar.me>
2025-06-02 21:49:14 +0200

    Update pre-push.sh.

--- test/pre-push.sh
+++ test/pre-push.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 echo 'Running tests...'
-vim --not-a-term --clean -n                             \
-  --cmd 'set rtp+=~/.vim/pack/deve/start/colortemplate' \
-  -c 'let g:autotest=1'                                 \
-  -c 'source%'                                          \
+vim --not-a-term -N -u NONE -n                           \
+  --cmd 'set rtp+=~/.vim/pack/devel/start/colortemplate' \
+  --cmd 'set rtp+=~/.vim/pack/devel/start/libcolor'      \
+  --cmd 'set rtp+=~/.vim/pack/devel/start/libparser'     \
+  --cmd 'set rtp+=~/.vim/pack/devel/start/libpath'       \
+  --cmd 'set rtp+=~/.vim/pack/devel/start/librelalg'     \
+  --cmd 'set rtp+=~/.vim/pack/devel/start/libtinytest'   \
+  --cmd 'set rtp+=~/.vim/pack/devel/start/libversion'    \
+  -c 'filetype plugin on'                                \
+  -c 'syntax on'                                         \
+  -c 'let g:autotest=1'                                  \
+  -c 'source%'                                           \
   $HOME/.vim/pack/devel/start/colortemplate/test/runtests.vim >/dev/null 2>&1
 
